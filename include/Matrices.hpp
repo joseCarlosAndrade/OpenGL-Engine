7847ce4a5d3e3bb8d1761804f80675f95dcfde2c
joseCarlosAndrade <josecarlos.101@usp.br>
2023-09-14 17:17:38 -0300

    matrix almost done

--- include/Matrices.hpp
+++ include/Matrices.hpp
@@ -1,14 +1,15 @@
 #include<iostream>
 #include<cstdlib>
 #include<assert.h>
+#include<cmath>
 
-class UniversalMatrix4x4f  {
+class GenMatrix4x4f  {
     protected:
         float matrix[16]; // change this to be generic
         unsigned int rows, columns;
     public:
-        /* Universal Constructo. Initializes matrix as zero. */
-        UniversalMatrix4x4f(unsigned int mrows = 4, unsigned int mcolumns = 4) :
+        /* Universal Constructor. Initializes matrix as zero. */
+        GenMatrix4x4f(unsigned int mrows = 4, unsigned int mcolumns = 4) :
         rows(mrows), columns(mcolumns) {
             
             for(auto &i : matrix) {
@@ -16,11 +17,17 @@ class UniversalMatrix4x4f  {
             }
         }
 
-        /* sets this matrix with size_t elements from the given vector.  */
-        void setMatrix(float vector[], unsigned int size_t) {
+        /* Resize matrix (avoid using this, it may make you loose data) */
+        void resize(unsigned int nrows, unsigned int ncolumns) {
+            rows = nrows;
+            columns = ncolumns;
+        }
+
+        /* Initializes this matrix with size_t elements from the given array. */
+        void setMatrix(float array[], unsigned int size_t) {
             assert(size_t < rows*columns +1);
             for (int i = 0; i < size_t; i++) {
-                matrix[i] = vector[i];
+                matrix[i] = array[i];
             }
         }
 
@@ -35,11 +42,38 @@ class UniversalMatrix4x4f  {
         void print() {
             for (int i = 0; i < rows; i ++) {
                 for (int  j = 0; j < columns; j++) {
-                    std::cout << this->operator()(i, j) << " ";
+                    std::cout << (*this)(i, j) << " ";
                 }
                 std::cout << std::endl;
             }std::cout << std::endl;
         }
+
+        /* Gets the transpose matrix from this obj */
+        GenMatrix4x4f getTranspose() {
+            GenMatrix4x4f m(columns, rows);
+
+            for (int i =0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    m(j, i) = (*this)(i, j);
+                }
+            }
+            return m;
+        }
+
+        int getRows() {
+            return rows;
+        }
+
+        int getColumns() {
+            return columns;
+        }
+
+        float & operator[](std::pair<int , int> index){
+            assert(index.first < 4 && index.second < 4);
+            return (matrix[index.first*4 + index.second]);
+            
+        }
+
 
         // overloading () operator to make indexing: matrix(i, j) 
         float& operator()(unsigned int i, unsigned int j) {
@@ -47,54 +81,208 @@ class UniversalMatrix4x4f  {
             return (matrix[i*4 + j]);
         }
 
-        UniversalMatrix4x4f operator+(UniversalMatrix4x4f& op) {
+        GenMatrix4x4f operator+(GenMatrix4x4f& op) {
             assert(rows == op.rows && columns == op.columns);
 
-            UniversalMatrix4x4f m(rows, columns);
+            GenMatrix4x4f m(rows, columns);
 
             for (int i =0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
-                    m(i, j) = this->operator()(i, j) + op(i, j);
+                    m(i, j) = (*this)(i, j) + op(i, j);
                 }
             }
             return m;
         }
 
-        UniversalMatrix4x4f operator*(UniversalMatrix4x4f& op) {
+        GenMatrix4x4f operator-(GenMatrix4x4f& op) {
+            assert(rows == op.rows && columns == op.columns);
+
+            GenMatrix4x4f m(rows, columns);
+
+            for (int i =0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    m(i, j) = (*this)(i, j) - op(i, j);
+                }
+            }
+            return m;
+        }
+
+        GenMatrix4x4f operator*(GenMatrix4x4f& op) {
+            std::cout << "assertion : " << columns << " " << op.rows << std::endl;
             assert(columns == op.rows);
 
-            UniversalMatrix4x4f r(rows, op.columns);
+            GenMatrix4x4f r(rows, op.columns);
 
             for ( int i = 0; i < rows ; i++) {
                 for (int j = 0; j< op.columns ; j++) {
 
                     for (int o = 0 ; o < columns ; o++) {
-                        r(i, j) += this->operator()(i, o) * op(o, j);
+                        r(i, j) += (*this)(i, o) * op(o, j);
                     }
                 }
             }
 
             return r;
         }
+
+        GenMatrix4x4f operator*(float scalar) {
+            GenMatrix4x4f m(rows, columns);
+
+            for (int i =0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    m(i, j) *= scalar;
+                }
+            }
+            return m;
+        }
+
+        
 };
 
-class MatrixTranslation : public UniversalMatrix4x4f {
+/* ## Identity Matrix*/
+class MatrixIdentity : public GenMatrix4x4f {
+    public:
+        MatrixIdentity(unsigned int size=4) {
+            this->resize(size, size);
 
+            for (int i = 0; i < rows; i++) {
+                (*this)(i,i) = 1;
+            }
+        }
 };
 
-class MatrixRotation : public UniversalMatrix4x4f {
+/* ## Translation matrix 
+Inherits Universal Matrix to handle translation spacial transformations on the 3D Space.
+*/
+class MatrixTranslation : public GenMatrix4x4f {
+    private:
+        float *tx, *ty, *tz;
 
+    public:
+        MatrixTranslation(unsigned int size=4, float t_x=0, float t_y=0, float t_z=0) {
+            this->resize(size, size);
+
+            for (int i = 0; i < rows; i++) {
+                (*this)(i,i) = 1;
+            }
+
+            tx = &(*this)(0, 3);
+            ty = &(*this)(1, 3);
+            tz = &(*this)(2, 3);
+
+            *tx = t_x;
+            *ty = t_y;
+            *tz = t_z;
+        }
+
+        void x(float x) {
+            *tx = x;
+        }
+
+        void y(float y) {
+            *ty = y;
+        }
+
+        void z(float z) {
+            *tz = z;
+        }
+            
+        
 };
 
-class MatrixScale : public UniversalMatrix4x4f {
+/* Base class to be inherited by all rotation matrices */
+class GenRotMat : public GenMatrix4x4f {
+    protected:
+        float ang;
+    public:
+        GenRotMat(unsigned int size=4, float angle) :  ang(angle) {
+            this->resize(size, size);
 
+            for (int i = 0; i < rows; i++) {
+                (*this)(i,i) = 1;
+            }
+        }
+    
 };
 
-class MatrixIdentity : public UniversalMatrix4x4f {
+class MatrixRotationX : public GenRotMat {
+    public:
+        void rot(float ang) {
+            float s = sin(ang);
+            float c = cos(ang);
 
+            (*this)(1, 1) = c;
+            (*this)(2, 2) = c;
+            (*this)(1, 2) = s;
+            (*this)(2, 1) = -s;
+        }
+
+        
 };
 
-class Matrix : public UniversalMatrix4x4f {
+class MatrixRotationY : public GenRotMat {
+    public:
+        void rot(float ang) {
+            float s = sin(ang);
+            float c = cos(ang);
+
+            (*this)(0, 0) = c;
+            (*this)(0, 2) = -s;
+            (*this)(2, 0) = s;
+            (*this)(2, 2) = c;
+        }
+
+        
+};
+
+class MatrixRotationZ : public GenRotMat {
+    public:
+        void rot(float ang) {
+            float s = sin(ang);
+            float c = cos(ang);
+
+            (*this)(0, 0) = c;
+            (*this)(0, 1) = -s;
+            (*this)(1, 0) = s;
+            (*this)(1, 1) = c;
+        }
+        
+};
+
+class MatrixScale : public GenMatrix4x4f {
+    private:
+        float *sx, *sy, *sz;
+    public:
+        MatrixScale(unsigned int size=4, float s_x =1, float s_y = 1, float s_z = 1) {
+            this->resize(size, size);
+
+            for (int i = 0; i < rows; i++) {
+                (*this)(i,i) = 1;
+            }
+            sx = &(*this)(0, 3);
+            sy = &(*this)(1, 3);
+            sz = &(*this)(2, 3);
+
+            *sx = s_x;
+            *sy = s_y;
+            *sz = s_z;
+        }
+
+        void x(float x) {
+            *sx = x;
+        }
+
+        void y(float y) {
+            *sy = y;
+        }
+
+        void z(float z) {
+            *sz = z;
+        }
+};
+
+
+class Matrix : public GenMatrix4x4f {
 
 };
 
--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -5,8 +5,8 @@
 int main() {
     float a[2] = {1, 2};
     float b[3] = { 5, 5, 5};
-    UniversalMatrix4x4f mat(4, 4);
-    UniversalMatrix4x4f mat_b(4, 4);
+    GenMatrix4x4f mat(4, 4);
+    GenMatrix4x4f mat_b(4, 4);
     mat(0, 3) = 1;
     // std::cout << mat. << std::endl;
     mat.setMatrix(a, 2);
@@ -14,13 +14,23 @@ int main() {
     mat.printM();
     mat_b.printM();
 
-    UniversalMatrix4x4f mat_sum = mat + mat_b;
+    GenMatrix4x4f mat_sum = mat + mat_b;
     mat_sum.printM();
-    UniversalMatrix4x4f point(4, 1);
+    GenMatrix4x4f point(4, 1);
     point(0, 0) = 1;
     point(1, 0) = 3;
     point(2, 0) = 2;
     point.print();
-    UniversalMatrix4x4f mat_mult = mat*point;
+    GenMatrix4x4f mat_mult = mat*mat;
+    // std::cout << "mat mult r c: " << mat_mult.getRows() << " " << mat_mult.getColumns() << std::endl;
+    // mat_mult = mat_mult*point;
+    mat.print();
     mat_mult.print();
+
+    MatrixIdentity identity(3);
+    MatrixRotation rotation(4);
+
+    identity.print();
+    rotation.print();
+
 }
